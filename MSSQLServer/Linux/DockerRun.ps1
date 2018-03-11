8338ba0dba8a3662a67fc1f95283dd3106d57be4
Alejandro Almeida <alejandro.almeida@intelequia.com>
2018-03-11 07:44:44 +0100

    Minimal fixes

---
+++ MSSQLServer/Linux/DockerRun.ps1
@@ -0,0 +1,1 @@
+﻿docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=P@ssw0rd' -p 1433:1433 -d microsoft/mssql-server-linux:2017-latest
--- dnn-platform-9.1.1.129-232/DockerRun.ps1
+++ dnn-platform-9.1.1.129-232/DockerRun.ps1
@@ -1,2 +1,2 @@
 ﻿# Run a DNN-Platform container 
-docker run -d -p 80:80 --name Hotelequia-Bot-Backend dnn-platform:9.1.1.129-232 
+docker run -d -p 80:8000 --name dnn-dockerized dnn-platform:9.1.1.129-232
